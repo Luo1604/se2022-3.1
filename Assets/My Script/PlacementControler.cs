62df6671f74f68c0fb0d29664049079a7caf8a57
hanging <114472721+hawng289@users.noreply.github.com>
2022-12-01 13:25:17 +0700

    Code đầu

---
+++ Assets/My Script/PlacementControler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARAnchorManager))]
+[RequireComponent(typeof(ARPlaneManager))]
+public class PlacementControler : MonoBehaviour
+
+{
+
+    [SerializeField]
+    private GameObject prefabToPlace;
+    
+    ARAnchorManager anchorManager;
+    ARRaycastManager arRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    public void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+        anchorManager = GetComponent<ARAnchorManager>();
+
+    }
+
+    private bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = Vector2.zero;
+        return false;
+    }
+
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+
+        if (arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            CreateAnchor(hits[0]);
+        } else
+        {
+            SSTools.ShowMessage("Bạn phải đặt model vào mặt phẳng đã xác nhận", SSTools.Position.bottom, SSTools.Time.twoSecond);
+        }
+    }
+
+    private ARAnchor CreateAnchor(in ARRaycastHit hit)
+    {
+        ARAnchor anchor;
+        ARPlane hitPlane = (ARPlane)hit.trackable;
+
+        anchorManager.anchorPrefab = prefabToPlace;
+        anchor = anchorManager.AttachAnchor(hitPlane, hit.pose);
+        anchorManager.anchorPrefab = null;
+        return anchor;
+    }
+}
