1f352012ac4f4d27838f9347c499ef11bcf35c13
KomaHomu <ssjggoku96@gmail.com>
2022-12-05 19:37:45 +0700

    added basic tutorial
    first time opening tutorial, need testing

---
+++ Assets/My Script/TutorialManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    void Start() 
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++) {
+            if (i == popUpIndex) {
+                popUps[popUpIndex].SetActive(true);
+            } else {
+                popUps[i].SetActive(false);
+            }
+        }
+
+        ClickEvent();
+
+        if (popUpIndex == popUps.Length) { 
+            popUps[popUpIndex - 1].SetActive(false);
+            this.enabled = false;
+        }
+    }
+
+    void ClickEvent() {
+        if (Input.GetMouseButtonDown(0)) {
+            popUpIndex++;
+        }
+    }
+}
