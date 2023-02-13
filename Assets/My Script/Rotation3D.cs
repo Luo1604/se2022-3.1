77f848e61f5d717b24700c4e684ad46972f28390
Thu Hien <117459480+thhn-uuie@users.noreply.github.com>
2023-02-13 21:12:05 +0700

    rotating object around the x axis, y axis

--- Assets/My Script/Rotation3D.cs
+++ Assets/My Script/Rotation3D.cs
@@ -4,28 +4,42 @@ using UnityEngine;
 
 public class Rotation3D : MonoBehaviour
 {
-
     private Touch touch;
-    private Vector2 touchPosition;
-    private Quaternion rotationY;
-
-    private float rotateSpeedModifier = 0.1f;
-
-
-    void Update() {
-        if (Input.touchCount > 0) {
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
+    [SerializeField] private float distanceToTarget = 10;
+    
+    private Vector3 previousPosition;
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
             touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                previousPosition = cam.ScreenToViewportPoint(touch.position);
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 newPosition = cam.ScreenToViewportPoint(touch.position);
+                Vector3 direction = previousPosition - newPosition;
+                
+                float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+                float rotationAroundXAxis = direction.y * 180; // camera moves vertically
 
-            if (touch.phase == TouchPhase.Moved) {
-                rotationY = Quaternion.Euler(
-                    touch.deltaPosition.y * rotateSpeedModifier,
-                    -touch.deltaPosition.x * rotateSpeedModifier,
-                    0f);
-                transform.rotation = rotationY * transform.rotation;
+
+                cam.transform.position = new Vector3(0,1,0);
+
+                rotationAroundXAxis = Mathf.Clamp(rotationAroundXAxis, -1, 1);
+
+                cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+                cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
+
+                cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+                previousPosition = newPosition;
             }
         }
     }
-
 }
 
-    
